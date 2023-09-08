9e929ca88c7d04886c389b256eab15c6697b72dc
Mateo <mateo.navarro98@gmail.com>
2023-09-08 12:10:59 -0300

    Modificaciones en vista producto

--- src/components/Content.js
+++ src/components/Content.js
@@ -20,94 +20,111 @@ const Content = () => {
   console.log("imgs--->", product.urlImg);
 
   return (
-    <div style={{ textAlign: "center" }} >
-      <div class="row">
-        <div class="col-sm-6 mb-3 mb-sm-0">
-          <div class="card">
-            <div class="card-body">
-              <div
-                id="carouselExampleIndicators"
-                class="carousel carousel-dark slide"
-              >
-                <div class="carousel-indicators">
-                  <button
-                    type="button"
-                    data-bs-target="#carouselExampleDark"
-                    data-bs-slide-to="0"
-                    class="active"
-                    aria-current="true"
-                    aria-label="Slide 1"
-                  ></button>
-                  <button
-                    type="button"
-                    data-bs-target="#carouselExampleDark"
-                    data-bs-slide-to="1"
-                    aria-label="Slide 2"
-                  ></button>
-                </div>
-                <div class="carousel-inner" style={{ maxWidth: "60%" }}>
-                  <div class="carousel-item active">
+    <div style={{margin:"0 auto" , display:"flex" , alignItems:"center"}}>
+      <div style={{ textAlign: "center", width: "80%",textAlign:"center", alignItems:"center"}}>
+        <h2>{product.name}</h2>
+        <div class="row">
+          <div class="col-sm-6 mb-3 mb-sm-0">
+            <div class="card" style={{width:"80%"}}>
+              <div class="card-body" >
+                <div
+                  id="carouselExampleIndicators"
+                  class="carousel carousel-dark slide"
+                >
+                  <div class="carousel-indicators">
+                    <button
+                      type="button"
+                      data-bs-target="#carouselExampleDark"
+                      data-bs-slide-to="0"
+                      class="active"
+                      aria-current="true"
+                      aria-label="Slide 1"
+                    ></button>
+                    <button
+                      type="button"
+                      data-bs-target="#carouselExampleDark"
+                      data-bs-slide-to="1"
+                      aria-label="Slide 2"
+                    ></button>
+                  </div>
+                  <div class="carousel-inner" >
+
+                  {/* {product.urlImg[0] ? product.urlImg.map((img)=>{
+                    <div class="carousel-item active">
                     <img
-                      src="https://www.camisetasclubes.com/shop/images/Real-Madrid/HMOGSHZ24.jpg"
+                      src={img}
                       class="d-block w-100"
                       alt="..."
                     />
                   </div>
+                  }) : "hola"} */}
 
-                  <div class="carousel-item active">
-                    <img
-                      src="https://www.camisetasclubes.com/shop/images/Real-Madrid/HMOGSHZ24_1.jpg"
-                      class="d-block w-100"
-                      alt="..."
-                    />
-                  </div>
+                     <div class="carousel-item active">
+                      <img
+                        src="https://acdn.mitiendanube.com/stores/216/721/products/photoroom-20220420_092710-5342fc330e2a5cbc2716504584747851-1024-1024.webp"
+                        class="d-block w-100"
+                        alt="..."
+                      />
+                    </div>
 
-                  {/* <div class="carousel-item">
+                    <div class="carousel-item active">
+                      <img
+                        src="https://acdn.mitiendanube.com/stores/216/721/products/photoroom-20220420_092637-25e7dcca9b63ef54b616504584747652-1024-1024.webp"
+                        class="d-block w-100"
+                        alt="..."
+                      />
+                    </div>
+
+                    {/* <div class="carousel-item">
                     <img src="..." class="d-block w-100" alt="..." />
                   </div> */}
+                  </div>
+                  <button
+                    class="carousel-control-prev"
+                    type="button"
+                    data-bs-target="#carouselExampleIndicators"
+                    data-bs-slide="prev"
+                  >
+                    <span
+                      class="carousel-control-prev-icon"
+                      aria-hidden="true"
+                    ></span>
+                    <span class="visually-hidden">Anterior</span>
+                  </button>
+                  <button
+                    class="carousel-control-next"
+                    type="button"
+                    data-bs-target="#carouselExampleIndicators"
+                    data-bs-slide="next"
+                  >
+                    <span
+                      class="carousel-control-next-icon"
+                      aria-hidden="true"
+                    ></span>
+                    <span class="visually-hidden">Siguiente</span>
+                  </button>
                 </div>
-                <button
-                  class="carousel-control-prev"
-                  type="button"
-                  data-bs-target="#carouselExampleIndicators"
-                  data-bs-slide="prev"
-                >
-                  <span
-                    class="carousel-control-prev-icon"
-                    aria-hidden="true"
-                  ></span>
-                  <span class="visually-hidden">Anterior</span>
-                </button>
-                <button
-                  class="carousel-control-next"
-                  type="button"
-                  data-bs-target="#carouselExampleIndicators"
-                  data-bs-slide="next"
-                >
-                  <span
-                    class="carousel-control-next-icon"
-                    aria-hidden="true"
-                  ></span>
-                  <span class="visually-hidden">Siguiente</span>
-                </button>
               </div>
             </div>
           </div>
-        </div>
-        <div class="col-sm-6">
-          <div class="card">
-            <div class="card-body">
-              <h4 class="card-title"> {product.name + " - " + product.team}</h4>
-              <h3 class="card-title"> {product.price}$</h3>
+          <div class="col-sm-6">
+            <div class="card" style={{width:"100%"}}>
+              <div class="card-body">
+                <h4 class="card-title">
+                  {" "}
+                  {product.name + " - " + product.team}
+                </h4>
+                <h3 class="card-title"> {product.price}$</h3>
 
-              <p class="card-text">{product.description}</p>
-              <p class="card-text">
-                {/* Talles disponibles: {sizeSetter(product.size)} */}
-              </p>
-              <div class="d-grid gap-2">
-                <button class="btn btn-primary" type="button">
-                  Agregar al carrito
-                </button>
+                <p class="card-text">{product.description}</p>
+                <p class="card-text">
+                  {/* Talles disponibles: {sizeSetter(product.size)} */}
+                </p>
+                <div class="d-grid gap-2">
+                  <button class="btn btn-primary" type="button">
+                    Agregar al carrito
+                  </button>
+                </div>
               </div>
             </div>
           </div>
