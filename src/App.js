9ceba4cec1aa2c5f655c5800c2662ad11fbdf3e8
Dario Beratti <darioberatti@gmail.com>
2023-09-08 12:16:59 -0300

    Se agregó logo, color de fondo y dropdown  a la Navbar

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React from "react";
 import { Route, Routes } from "react-router";
 import "./App.css";
 import Grid from "./components/Grid";
@@ -28,8 +28,6 @@ function App() {
       .get("http://localhost:3001/api/products")
       .then((response) => setProducts(response.data));
   }, []);
-
-  console.log(usuario);
 
   return (
     <div>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,23 +1,25 @@
 import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
-
+import LogoRFC from "../assets/Black White Modern Concept Football Club Logo.png";
 
 const Navbar = () => {
-  const usuario = useSelector((state)=> state.user.value)
+  const usuario = useSelector((state) => state.user.value);
 
   return (
     <>
-      <nav class="navbar navbar-expand-lg bg-body-tertiary">
+      <nav
+        className="navbar navbar-expand-lg"
+        style={{ backgroundColor: "lightgray" }}
+      >
         <div class="container-fluid">
           <Link to={"/"}>
-            <a class="navbar-brand">
-              <img
-                src="" //Aquí va el logo de RFC
-                alt="RFC"
-                width="30"
-                height="24"
-              />
-            </a>
+            <img
+              class="navbar-brand"
+              src={LogoRFC}
+              alt="RFC"
+              width="60"
+              height="70"
+            />
           </Link>
           <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <form class="d-flex" role="search">
@@ -34,11 +36,36 @@ const Navbar = () => {
           </div>
           <div>
             {usuario.name ? (
-              <Link>
-                <button type="button" class="btn btn-dark">
+              <>
+                <button
+                  class="nav-link dropdown-toggle"
+                  href="#"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
                   <strong>{usuario.name}</strong>
                 </button>
-              </Link>
+                <ul class="dropdown-menu">
+                  <li>
+                    <button class="dropdown-item" href="#">
+                      Ver tu carrito
+                    </button>
+                  </li>
+                  <li>
+                    <button class="dropdown-item" href="#">
+                      Perfil
+                    </button>
+                  </li>
+                  <li>
+                    <hr class="dropdown-divider" />
+                  </li>
+                  <li>
+                    <button class="dropdown-item" href="#">
+                      Desloguearse
+                    </button>
+                  </li>
+                </ul>
+              </>
             ) : (
               <Link to={"/login"}>
                 <button type="button" class="btn btn-dark">
@@ -47,6 +74,7 @@ const Navbar = () => {
               </Link>
             )}
           </div>
+          <div style={{ marginRight: "10px" }}></div>
         </div>
       </nav>
     </>
